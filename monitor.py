76e5de2656040e7458457a33800d59f65c478720
mistercaste <mistercaste@gmail.com>
2025-12-18 20:25:45 +0100

    Update monitor.py
     APIv2 and MQTT authentication

--- monitor.py
+++ monitor.py
@@ -1,28 +1,45 @@
 import os
 import telebot
 import paho.mqtt.client as mqtt
+from paho.mqtt.enums import CallbackAPIVersion
 
-# Configure via environment variables
+# Environment variables
 TOKEN = os.getenv('TELEGRAM_TOKEN')
 CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 MQTT_BROKER = os.getenv('MQTT_BROKER', 'localhost')
-MQTT_TOPIC = os.getenv('MQTT_TOPIC', 'security/vulnerabilities')
+MQTT_PORT = int(os.getenv('MQTT_PORT', 1883))
+MQTT_TOPIC = os.getenv('MQTT_TOPIC', 'security/#')
+MQTT_USER = os.getenv('MQTT_USER')
+MQTT_PASS = os.getenv('MQTT_PASS')
 
 bot = telebot.TeleBot(TOKEN)
 
-def on_connect(client, userdata, flags, rc):
-    print(f"Connected to MQTT broker with result: {rc}")
-    client.subscribe(MQTT_TOPIC)
+def on_connect(client, userdata, flags, rc, properties=None):
+    if rc == 0:
+        print(f"INFO - Connected to the broker: {MQTT_BROKER}")
+        client.subscribe(MQTT_TOPIC)
+    else:
+        print(f"ERROR - Connection error code: {rc}")
 
 def on_message(client, userdata, msg):
     payload = msg.payload.decode()
-    message_text = f"WARNING - Vulnerability found!*\n\n\tTopic:* {msg.topic}\n💬 *Message:* {payload}"
-    print(f"Sending notification: {payload}")
-    bot.send_message(CHAT_ID, message_text, parse_mode='Markdown')
+    message_text = f"INFO - MQTT message found.\n\tTopic: {msg.topic}\n\tMessage: {payload}"
+    print(f"INFO - Message received: {payload}")
+    try:
+        bot.send_message(CHAT_ID, message_text, parse_mode='Markdown')
+    except Exception as e:
+        print(f"ERROR - Sending to Telegram: {e}")
 
-client = mqtt.Client()
+client = mqtt.Client(callback_api_version=CallbackAPIVersion.VERSION2)
+if MQTT_USER and MQTT_PASS:
+    client.username_pw_set(MQTT_USER, MQTT_PASS)
+
 client.on_connect = on_connect
 client.on_message = on_message
 
-client.connect(MQTT_BROKER, 1883, 60)
-client.loop_forever()
+try:
+    print(f"INFO - Connection tentative to: {MQTT_BROKER}...")
+    client.connect(MQTT_BROKER, MQTT_PORT, 60)
+    client.loop_forever()
+except Exception as e:
+    print(f"ERROR - {e}")
